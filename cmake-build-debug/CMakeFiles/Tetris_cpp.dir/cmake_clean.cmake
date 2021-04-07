db75c9c0d9f5fba0479a8855ea86b39b0c7e595c
GacinhoV33 <fgacek@student.agh.edu.pl>
2021-04-07 16:11:29 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Tetris_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris_cpp.dir/main.cpp.obj"
+  "Tetris_cpp.exe"
+  "Tetris_cpp.exe.manifest"
+  "Tetris_cpp.pdb"
+  "libTetris_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by gacek on 7 kwi 2021.
+//
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+int main(){
+
+    return 0;
+};
